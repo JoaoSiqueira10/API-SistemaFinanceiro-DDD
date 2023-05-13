0ebb7d8316671babccbdc394a8f6c6e4ffca1f56
Joao Guilherme <joao.guilherme100@hotmail.com>
2023-05-13 15:37:50 -0300

    Criando interface e repositorio genericos

---
+++ Infra/Configuracao/ContextBase.cs
@@ -0,0 +1,45 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuracao
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options)
+        {             
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro { set; get; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { set; get; }
+        public DbSet<Categoria> Categoria { set; get; }
+        public DbSet<Despesa> Despesa { set; get; }
+
+        //verifica se a URL esta direcionada para o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=DESKTOP-S42DPI7;Initial Catalog=DDD_Financeiro_2023;Integrated Security=True";
+        }
+    }
+}
