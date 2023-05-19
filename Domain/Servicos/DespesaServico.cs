d9d9f9295b6d7424bfaeb4ad8d10fb7a98a64b9d
Joao Guilherme <joao.guilherme100@hotmail.com>
2023-05-19 11:59:36 -0300

    Implementando validacao no Dominio

---
+++ Domain/Servicos/DespesaServico.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.InterfaceService;
+using Entities.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Servicos
+{
+    public class DespesaServico : IDespesaServico
+    {
+        private readonly InterfaceDespesa _interfaceDespesa;
+        public DespesaServico(InterfaceDespesa interfaceDespesa) 
+        {
+            _interfaceDespesa = interfaceDespesa;
+        }
+
+        public async Task AdicionarDespesa(Despesa despesa)
+        {
+            var data = DateTime.UtcNow;
+            despesa.DataAlteracao = data;
+            despesa.Ano = data.Year;
+            despesa.Mes = data.Month;
+
+            var valido = despesa.ValidarPropriedadeString(despesa.Nome, "Nome");
+            if (valido)
+            {
+                await _interfaceDespesa.Add(despesa);
+            }
+        }
+
+        public async Task AtualizarDespesa(Despesa despesa)
+        {
+            var data = DateTime.UtcNow;
+            despesa.DataAlteracao = data;
+
+            if (despesa.Pago)
+            {
+                despesa.DataPagamento = data;
+            }
+            
+            var valido = despesa.ValidarPropriedadeString(despesa.Nome, "Nome");
+            if (valido)
+            {
+                await _interfaceDespesa.Update(despesa);
+            }
+        }
+    }
+}
